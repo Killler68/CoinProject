13532a64fa46c2241b4d5055643c927f7b8d79e5
VIP <belevitin2010@Mail.ru>
2022-09-28 21:22:08 +0300

    Got rid of the SuppressLint annotation

--- app/src/main/java/com/example/coinproject/listcoin/item/ListCoinItem.kt
+++ app/src/main/java/com/example/coinproject/listcoin/item/ListCoinItem.kt
@@ -1,6 +1,5 @@
 package com.example.coinproject.listcoin.item
 
-import android.annotation.SuppressLint
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -15,7 +14,6 @@ class ListCoinItem(
     private val onClick: (String) -> Unit
 ) : AbstractBindingItem<RecyclerItemListCoinBinding>() {
 
-    @SuppressLint("SetTextI18n")
     override fun bindView(binding: RecyclerItemListCoinBinding, payloads: List<Any>) {
         super.bindView(binding, payloads)
 
@@ -30,13 +28,18 @@ class ListCoinItem(
 
             textCoinItem.text = coinData.name
             textCoinAbbreviatedItem.text = coinData.symbol
-            textCoinCourseItem.text = coinData.current_price.toString()
+
+            textCoinCourseItem.text = root.resources.getString(
+                R.string.usd, coinData.current_price.toString()
+            )
             if (coinData.market_cap_change_percentage_24h > 0) {
                 binding.textCoinCoursePercentItem.setTextColor(Color.GREEN)
             } else {
                 binding.textCoinCoursePercentItem.setTextColor(Color.RED)
             }
-            textCoinCoursePercentItem.text = "${coinData.market_cap_change_percentage_24h}%"
+            textCoinCoursePercentItem.text = root.resources.getString(
+                R.string.percentage, coinData.market_cap_change_percentage_24h.toString()
+            )
         }
     }
 
