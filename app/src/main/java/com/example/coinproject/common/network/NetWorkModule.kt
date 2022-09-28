ebbe34db075d03ea0b156f4e8d7b7a291c3238c1
VIP <belevitin2010@Mail.ru>
2022-09-28 20:43:04 +0300

    Took out provideApi and provideRetrofit in networkModule

--- app/src/main/java/com/example/coinproject/common/network/NetWorkModule.kt
+++ app/src/main/java/com/example/coinproject/common/network/NetWorkModule.kt
@@ -1,12 +1,18 @@
 package com.example.coinproject.common.network
 
+import com.example.coinproject.common.api.BASE_URL
+import com.example.coinproject.common.api.CoinApi
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import dagger.Module
 import dagger.Provides
+import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 class NetworkModule {
+
+    @Provides
+    fun provideApi(retrofit: Retrofit): CoinApi = retrofit.create(CoinApi::class.java)
 
     @Provides
     fun provideGsonConverterFactory(): GsonConverterFactory = GsonConverterFactory.create()
@@ -15,4 +21,17 @@ class NetworkModule {
     fun provideRetrofitRxJava(): RxJava2CallAdapterFactory {
         return RxJava2CallAdapterFactory.create()
     }
+
+    @Provides
+    fun provideRetrofit(
+        gsonConverterFactory: GsonConverterFactory,
+        rxJava2CallAdapterFactory: RxJava2CallAdapterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(gsonConverterFactory)
+            .addCallAdapterFactory(rxJava2CallAdapterFactory)
+            .build()
+    }
+
 }
--- app/src/main/java/com/example/coinproject/listcoin/ListCoinModule.kt
+++ app/src/main/java/com/example/coinproject/listcoin/ListCoinModule.kt
@@ -1,7 +1,6 @@
 package com.example.coinproject.listcoin
 
 import androidx.lifecycle.ViewModel
-import com.example.coinproject.common.api.BASE_URL
 import com.example.coinproject.common.api.CoinApi
 import com.example.coinproject.common.network.NetworkModule
 import com.example.coinproject.listcoin.repository.ListCoinRepository
@@ -9,34 +8,16 @@ import com.example.coinproject.listcoin.repository.ListCoinRepositoryImpl
 import com.example.coinproject.listcoin.usecase.ListCoinUseCase
 import com.example.coinproject.listcoin.usecase.ListCoinUseCaseImpl
 import com.example.coinproject.listcoin.viewmodel.ListCoinViewModel
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.ClassKey
 import dagger.multibindings.IntoMap
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 @Module(includes = [NetworkModule::class])
 class ListCoinModule {
 
     @Provides
-    fun provideApi(retrofit: Retrofit): CoinApi = retrofit.create(CoinApi::class.java)
-
-    @Provides
     fun provideRepository(coinApi: CoinApi): ListCoinRepository = ListCoinRepositoryImpl(coinApi)
-
-    @Provides
-    fun provideRetrofit(
-        gsonConverterFactory: GsonConverterFactory,
-        rxJava2CallAdapterFactory: RxJava2CallAdapterFactory
-    ): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(gsonConverterFactory)
-            .addCallAdapterFactory(rxJava2CallAdapterFactory)
-            .build()
-    }
 
     @Provides
     fun provideUseCase(repository: ListCoinRepository): ListCoinUseCase =
