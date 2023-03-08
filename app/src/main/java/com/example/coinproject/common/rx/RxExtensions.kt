f45394bd72c557ca99fe14f4f27d5661417bf832
VIP <belevitin2010@mail.ru>
2023-03-09 01:43:56 +0300

    Remove rxJava

--- app/src/main/java/com/example/coinproject/common/rx/RxExtensions.kt
+++
@@ -1,8 +0,0 @@
-package com.example.coinproject.common.rx
-
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-
-operator fun CompositeDisposable.plusAssign(disposable: Disposable) {
-    add(disposable)
-}
